f0e0d030ae147a5ddf405e5956e8f46ebfda77cb
Jnejsum <78685559+Jnejsum@users.noreply.github.com>
2023-03-08 17:09:03 +0100

    Small updates, added default rates excel file

--- model_functions.py
+++ model_functions.py
@@ -6,14 +6,26 @@
 
 # - setup (called by init): sets up model parameters
 
-class clo_model():
+# IMPORTS
+import pandas as pd
+
+class CLOModel():
     def __init__(self,**kwargs):
         self.setup(**kwargs) # set up parameters, by running function setup() on initialization
+        self.load_default() # load default rates
         
     def setup(self,**kwargs):
         #(1) Model parameters
         self.parameter = 1.0
-
+        
         #(2) Update baseline parameters using keywords
         for key,val in kwargs.items():
             setattr(self,key,val)
+
+    def load_default(self):
+        '''Loads S&P Cumulative Default Rates for 1981-2021 as a dataframe'''
+        self.default = pd.read_excel('data\default_rates_SP.xlsx', header = 2, index_col = "Rating")
+        
+    def GBM(self, ):
+        '''Geometric Brownian Motion'''
+        
