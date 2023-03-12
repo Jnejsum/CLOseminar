f6f6c6f73225dc2e924896ddf520860f38f931b9
Jnejsum <78685559+Jnejsum@users.noreply.github.com>
2023-03-12 11:54:09 +0100

    Added GBM function
    Implemented Geometric Brownian Motion function for use in the simulation.

--- .ipynb_checkpoints/model_functions-checkpoint.py
+++ .ipynb_checkpoints/model_functions-checkpoint.py
@@ -2,12 +2,15 @@
 # model_functions.py #
 ######################
 
-# This module contains the model functions class which contains the following functions:
+# This module contains the CLOModel class which contains the following functions:
 
 # - setup (called by init): sets up model parameters
+# - load_default: loads Cumulative Default Rates DataFrame
+# - GBM: draws an array of underlying asset value paths
 
 # IMPORTS
 import pandas as pd
+import numpy as np
 
 class CLOModel():
     def __init__(self,**kwargs):
@@ -16,8 +19,16 @@ class CLOModel():
         
     def setup(self,**kwargs):
         #(1) Model parameters
-        self.parameter = 1.0
+        self.parameter = 1.0 # test parameter
         
+        # Brownian Motion
+        self.S0 = 100 # asset value period 0
+        self.mu = 0.1 # drift coefficient
+        self.sigma = 0.3 # volatility
+        self.ttm = 5 # time to maturity
+        self.freq = 100 # number of steps (granularity of process)
+        self.n_sims = 100 # number of simulations
+         
         #(2) Update baseline parameters using keywords
         for key,val in kwargs.items():
             setattr(self,key,val)
@@ -26,6 +37,29 @@ class CLOModel():
         '''Loads S&P Cumulative Default Rates for 1981-2021 as a dataframe'''
         self.default = pd.read_excel('data\default_rates_SP.xlsx', header = 2, index_col = "Rating")
         
-    def GBM(self, ):
-        '''Geometric Brownian Motion'''
+    def GBM(self):
+        '''Geometric Brownian Motion
+        RETURNS:
+        Numpy array of dimensions (1 + number of simulations, total steps)
+        '''
+        # see: https://quantpy.com.au/stochastic-calculus/simulating-geometric-brownian-motion-gbm-in-python/
+        
+        # (1) define calculation parts
+        dt = self.ttm / self.freq # calculate each step (total time / frequency)
+        drift = self.mu - 0.5 * self.sigma ** 2
+        
+        # (2) draw and prepare array
+        np.random.seed(50) # set seed
+        normal = np.random.normal(loc=0, scale=np.sqrt(dt), size=(self.n_sims, self.freq)) # draw normal dist
+        St = np.exp(drift * dt + self.sigma * np.transpose(normal))
+        St = np.vstack([np.ones(self.n_sims), St])
+        
+        # use cumulative product (over rows) to calculate simulation paths, and multiply by initial value V0
+        return self.S0 * St.cumprod(axis=0) # axis=0 to calculate over rows
+        
+        
+        
+        
+        
+        
         
