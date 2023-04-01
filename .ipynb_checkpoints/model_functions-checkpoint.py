6718f100a22baaa56cc4b03b027d48ad8dcb3458
Jnejsum <78685559+Jnejsum@users.noreply.github.com>
2023-04-01 18:16:11 +0200

    Added face value calculation function, working on result table

--- .ipynb_checkpoints/model_functions-checkpoint.py
+++ .ipynb_checkpoints/model_functions-checkpoint.py
@@ -9,10 +9,12 @@
 # - GBM_fig: draws an array of underlying asset value paths for the purpose of plotting
 # - GBM: draws an array of underlying asset value paths for the CLO model
 # - SPV_value: simulates assets and calculates SPV values
+# - face_value: calculates the face value of a loan, given rating and maturity
 
 # IMPORTS
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
 class CLOModel():
     def __init__(self,**kwargs):
@@ -20,13 +22,10 @@ class CLOModel():
         self.load_default() # load default rates
         
     def setup(self,**kwargs):
-        # (1) Model parameters
-        self.parameter = 1.0 # test parameter
-        
-        # (2) Brownian Motion
+        # (1) Brownian Motion
         self.V0 = 100 # asset value period 0
-        self.mu = 0.02 # drift coefficient
-        self.sigma_m = 0.14 # market level variance
+        self.mu = 0.091 # drift coefficient
+        self.sigma_m = 0.8*0.14 # market level variance (0.8 stems from beta)
         self.sigma_j = 0.25 # firm level variance
         self.sigma = (self.sigma_m ** 2 + self.sigma_j ** 2) ** 0.5 # total variance parameter
         self.T = 5 # time to maturity
@@ -34,8 +33,10 @@ class CLOModel():
         self.n = 1000 # number of simulations
         self.j = 100 # number of loans
         
-        # (3) SPV cash flow and value calculations
-        self.B = 70 # face value default
+        # (2) SPV cash flow and value calculations
+        self.rating = 'B' # rating for each loan
+        self.default = self.load_default()
+        self.B = self.face_value() # call face value function to calculate
         
         # Update baseline parameters using keywords
         for key,val in kwargs.items():
@@ -43,7 +44,7 @@ class CLOModel():
 
     def load_default(self):
         '''Loads S&P Cumulative Default Rates for 1981-2021 as a dataframe'''
-        self.default = pd.read_excel('data\default_rates_SP.xlsx', header = 2, index_col = "Rating")
+        return pd.read_excel('data\default_rates_SP.xlsx', header = 2, index_col = "Rating")
         
     def GBM_fig(self):
         '''Geometric Brownian Motion for illustration purpose
@@ -59,15 +60,15 @@ class CLOModel():
         # (2) draw and prepare array
         np.random.seed(50) # set seed
         W = np.random.normal(loc=0, scale=np.sqrt(dt), size=(self.n, self.m)) # draw normal dist
-        St = np.exp(drift * dt + self.sigma * np.transpose(W))
-        St = np.vstack([np.ones(self.n), St])
+        Vt = np.exp(drift * dt + self.sigma * np.transpose(W))
+        Vt = np.vstack([np.ones(self.n), Vt])
     
         #Might want to do this to save the normal draws too:
         # if return_normal:
             # return self.S0 * St.cumprod(axis=0), normal
             
         # use cumulative product (over rows) to calculate simulation paths, and multiply by initial value V0
-        return self.S0 * St.cumprod(axis=0) # axis=0 to calculate over rows
+        return self.V0 * Vt.cumprod(axis=0) # axis=0 to calculate over rows
     
     def GBM(self):
         '''Geometric Brownian Motion for CLO model        
@@ -99,9 +100,16 @@ class CLOModel():
         CF_sum = np.sum(CF, axis=1) # take sum over firms j = 1, ..., J
         
         return np.sort(CF_sum)
+    
+    def face_value(self):
+        '''Face value from cumulative default probability table
+        RETURNS:
+        Face value B
+        '''
+        def_prob = self.default.loc[self.rating, self.T]/100 # cumulative default probability from table        
         
-        
-        
+        return self.V0 / np.exp( - norm.ppf(def_prob) * self.sigma * np.sqrt(self.T)
+                                 - ( self.mu - 0.5 * self.sigma ** 2 ) * self.T )
         
         
         
