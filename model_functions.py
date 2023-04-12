b3da6d7687693cbc1a4a3b46f06974b2371f56a1
Jnejsum <78685559+Jnejsum@users.noreply.github.com>
2023-04-12 18:05:41 +0200

    Various small changes, figures updated

--- model_functions.py
+++ model_functions.py
@@ -23,10 +23,10 @@ class CLOModel():
         self.load_default() # load default rates
         
     def setup(self, **kwargs):
-        # (1) Brownian Motion
         self.V0 = 100 # asset value period 0
         self.rf = 0.004 # risk free interest rate
-        self.c = 0.05 # risk premium parameter
+        self.c = 0.07 # risk premium parameter
+        self.gamma = 1 # scalar on rf in drift calculation      
         self.sigma_m = 0.8*0.14 # market level variance (0.8 stems from beta)
         self.sigma_j = 0.25 # firm level variance
         self.sigma = (self.sigma_m ** 2 + self.sigma_j ** 2) ** 0.5 # total variance parameter
@@ -34,11 +34,8 @@ class CLOModel():
         self.m = 100 # number of steps (granularity of process)
         self.n = 10000 # number of simulations
         self.j = 100 # number of loans
-        
-        # (2) SPV cash flow and value calculations
         self.rating = 'B' # rating for each loan
-        self.default = self.load_default()
-        self.B = self.face_value() # call face value function to calculate
+        self.default = self.load_default() # default probability table
         
         # Update baseline parameters using keywords
         for key,val in kwargs.items():
@@ -57,19 +54,15 @@ class CLOModel():
 
         # (1) define calculation parts
         dt = self.T / self.m # calculate each step (total time / frequency)
-        drift = (self.rf + self.c) - 0.5 * self.sigma ** 2
+        drift = (self.c + self.gamma * self.rf) - 0.5 * self.sigma ** 2
         
         # (2) draw and prepare array
         np.random.seed(50) # set seed
         W = np.random.normal(loc=0, scale=np.sqrt(dt), size=(self.n, self.m)) # draw normal dist
         Vt = np.exp(drift * dt + self.sigma * np.transpose(W))
         Vt = np.vstack([np.ones(self.n), Vt])
-    
-        #Might want to do this to save the normal draws too:
-        # if return_normal:
-            # return self.S0 * St.cumprod(axis=0), normal
             
-        # use cumulative product (over rows) to calculate simulation paths, and multiply by initial value V0
+        # (3) use cumulative product (over rows) to calculate simulation paths, and multiply by initial value V0
         return self.V0 * Vt.cumprod(axis=0) # axis=0 to calculate over rows
     
     def GBM(self, risk_neutral=False):
@@ -80,7 +73,7 @@ class CLOModel():
         
         # (1) define calculation parts
         if risk_neutral: drift = self.rf - 0.5 * self.sigma ** 2
-        else: drift = (self.rf + self.c) - 0.5 * self.sigma ** 2
+        else: drift = (self.c + self.gamma * self.rf) - 0.5 * self.sigma ** 2
 
         # (2) draw and prepare array
         np.random.seed(2020) # set seed
@@ -101,7 +94,8 @@ class CLOModel():
         else: V = self.GBM()[-1,:,:] # take only terminal values
         
         # (2) calculate minimum and take sum
-        CF = np.minimum(V, self.B)
+        B = self.face_value()
+        CF = np.minimum(V, B)
         CF_sum = np.sum(CF, axis=1) # take sum over firms j = 1, ..., J
         
         return np.sort(CF_sum)
@@ -112,10 +106,10 @@ class CLOModel():
         Face value B
         '''
         
-        def_prob = self.default.loc[self.rating, self.T]/100 # cumulative default probability from table        
+        def_prob = self.default.loc[self.rating, self.T] / 100 # cumulative default probability from table        
         
         return self.V0 / np.exp( - norm.ppf(def_prob) * self.sigma * np.sqrt(self.T)
-                                 - ( (self.rf + self.c) - 0.5 * self.sigma ** 2 ) * self.T )
+                                 - ( (self.c + self.gamma * self.rf) - 0.5 * self.sigma ** 2 ) * self.T )
     
     def result_table(self):
         '''Creates result table
@@ -129,14 +123,13 @@ class CLOModel():
         
         # (2) add face value column
         SPV_values = self.SPV_value()
-        df['aggregate face value'] = np.quantile(SPV_values, df['default probability'].values/100) # quantile of sim. dist.
+        df['aggregate face value'] = np.quantile(SPV_values, df['default probability'].values / 100) # quantile of sim. dist.
         
         # (3) add market value column
         SPV_values_Q = self.SPV_value(risk_neutral=True) # risk neutral SPV values
-        
         for k in df.index: # for each rating
-            Bk = np.quantile(SPV_values, df.loc[k, 'default probability']/100) # tranche value
-            df.loc[k, 'aggregate market value'] = np.minimum(SPV_values_Q, Bk).mean() * np.exp(-self.rf * self.T)
+            Bk = np.quantile(SPV_values, df.loc[k, 'default probability'] / 100) # tranche value
+            df.loc[k, 'aggregate market value'] = np.minimum(SPV_values_Q, Bk).mean() * np.exp(- self.rf * self.T)
             
             
         # (4) tranching to find face value and market value
